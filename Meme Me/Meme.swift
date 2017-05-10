232524e0636b96c696b99c54db6354d446204da2
TigranTT <TigranTT@Tigrans-iMac-2.attlocal.net>
2017-05-09 23:10:49 -0500

    updates

---
+++ Meme Me/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  Meme Me
+//
+//  Created by Tigran Tshorokhyan on 4/16/17.
+//  Copyright © 2017 Tigran Tshorokhyan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    let topText : NSString!
+    let bottomText : NSString!
+    let originalImage : UIImage!
+    let memedImage : UIImage!
+}
